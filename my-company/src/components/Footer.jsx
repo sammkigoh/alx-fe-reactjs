74a2c792d1389d28982672c18b839575b3e254c4
Samm <sammkigoh@gmail.com>
2024-08-16 14:40:21 +0300

    updated my company website

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer
+			style={{
+				padding: "10px",
+				borderTop: "1px solid #ccc",
+				marginTop: "20px",
+				textAlign: "center",
+			}}
+		>
+			<p>&copy; 2024 My Company. All rights reserved.</p>
+		</footer>
+	);
+}
+
+export default Footer;
