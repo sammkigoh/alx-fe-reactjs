a25182eb17bd73613037fc1285e82c84a59083e6
Samm <sammkigoh@gmail.com>
2024-08-16 12:41:03 +0300

    updated the file paths

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import ProfilePage from "./components/ProfilePage";
-import UserContext from "./UserContext";
+import UserContext from "./components/UserContext";
 
 // import { WelcomeMessage } from "./components/WelcomeMessage";
 // // import { Header } from "./components/Header";
