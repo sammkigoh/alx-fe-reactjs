72ae0563ac9d12e0b352bbc66969dcc4a780119a
Samm <sammkigoh@gmail.com>
2024-08-16 12:20:18 +0300

    created a new directory alx-react-prop

---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const MainContent = () => {
+	return (
+		<div>
+			<main>
+				<p>I love to visit New York, Paris, and Tokyo.</p>
+			</main>
+		</div>
+	);
+};
+export default MainContent;
