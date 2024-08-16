467435b81c39808d9d7dd1a125fcd4f0c717dfea
Samm <sammkigoh@gmail.com>
2024-08-16 10:13:58 +0300

    duplicated the alx-react-app

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Header = () => {
+	return (
+		<div>
+			<header style={{ border: "1px solid gray" }}>
+				<h1>My Favorite Cities</h1>
+			</header>
+		</div>
+	);
+};
+export default Header;
