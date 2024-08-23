8e3a79066de191a56019bb735814048e68237083
Samm <sammkigoh@gmail.com>
2024-08-23 11:27:33 +0300

    changed the location of the recipeStore.js

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import useRecipeStore from "./store/recipeStore";
+import useRecipeStore from "./components/recipeStore";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import "./App.css";
