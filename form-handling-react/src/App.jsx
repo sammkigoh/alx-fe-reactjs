ccc08a1c0e9bfbe8600b18ae6bb9e54d536751d2
Samm <sammkigoh@gmail.com>
2024-09-01 18:39:36 +0300

    updated the files

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from "react";
-import RegistrationForm from "./components/RegistrationForm";
-import formikForm from "./components/formikForm";
+// import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/formikForm";
 import "./App.css";
 
 function App() {
 	return (
 		<div>
 			<h1>User Registration with Formik</h1>
-			<RegistrationForm />
-			<formikForm />
+			{/* <RegistrationForm /> */}
+			<FormikForm />
 		</div>
 	);
 }
