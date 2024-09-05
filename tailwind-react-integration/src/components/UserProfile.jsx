a23a361446485602dea46ae971decbe116e3a9db
Samm <sammkigoh@gmail.com>
2024-09-05 16:12:36 +0300

    added hover:shadow-xl

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function UserProfile() {
 	return (
-		<div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:drop-shadow-xl transition-shadow duration-300 ease-in-out">
+		<div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300 ease-in-out">
 			<img
 				src="https://via.placeholder.com/150"
 				alt="User"
