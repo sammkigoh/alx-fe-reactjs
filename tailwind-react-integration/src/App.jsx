529d000ddfae1db6769bfbedbce91fd0f7bb5f9c
Samm <sammkigoh@gmail.com>
2024-09-05 16:01:34 +0300

    responsive design implementation using Tailwind CSS

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -3,10 +3,10 @@ import UserProfile from "./components/UserProfile";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
-		
+		<div>
+			<UserProfile />
+		</div>
 	);
 }
 
