ab63538f21b7a69e755f9ca86813a4b99d0b3419
Samm <sammkigoh@gmail.com>
2024-09-20 09:44:17 +0300

    initial commit for Working with APIs project

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="container mx-auto p-4">
+			<h1 className="text-center text-2xl font-bold mb-6">
+				GitHub User Search
+			</h1>
+			{/* <SearchBar /> */}
+		</div>
+	);
+}
+
+export default App;
