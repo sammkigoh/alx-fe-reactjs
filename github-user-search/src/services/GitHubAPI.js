405be8daf9b662ae30ff5509ccb02fb89536be7e
Samm <sammkigoh@gmail.com>
2024-09-20 09:58:50 +0300

    added files in the components and services directory

---
+++ github-user-search/src/services/GitHubAPI.js
@@ -0,0 +1,6 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_GITHUB_API_KEY;
+const BASE_URL = "https://api.gihub.com";
+
+export const fetchGitHubUser = async (username) => {};
